403e18e06610ed011eef34297e7500880c546cd2
elyeandre <96169920+elyeandre@users.noreply.github.com>
2024-10-31 15:45:00 +0800

    feat: add Tournament schema inheriting from Event model

---
+++ src/models/Tournament.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const Event = require('./Event');
+
+// define the schema for Tournament
+const tournamentSchema = new mongoose.Schema(
+  {
+    tournamentFee: {
+      type: Number,
+      default: null
+    },
+    tournamentCategories: [
+      {
+        name: {
+          type: String,
+          required: true
+        },
+        participantLimit: {
+          type: Number,
+          required: true
+        }
+      }
+    ],
+    bracket: {
+      type: [
+        {
+          round: {
+            type: String, // e.g., "Quarterfinals", "Semifinals", "Finals"
+            required: true
+          },
+          matchups: [
+            {
+              participants: [
+                {
+                  name: {
+                    type: String,
+                    required: true
+                  },
+                  score: {
+                    type: Number,
+                    default: 0
+                  },
+                  userId: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'User'
+                  }
+                }
+              ],
+              winner: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+              }
+            }
+          ]
+        }
+      ],
+      default: []
+    }
+  },
+  {
+    timestamps: true,
+    strict: 'throw'
+  }
+);
+
+//tournament model, inheriting from Event
+const Tournament = Event.discriminator('Tournament', tournamentSchema);
+
+module.exports = Tournament;
