d6e7e61964b6b7ebe1a3fc596960632941ac8c8c
frootyjas <jasminedenriquez@gmail.com>
2024-11-12 00:10:34 +0800

    Revert "fix admin membership page"
    This reverts commit c199ebc946db7b0a62ac3457a16b1c8f3d159842.

--- client/js/pages/adminviewmembership/adminViewMembership.js
+++ client/js/pages/adminviewmembership/adminViewMembership.js
@@ -1,10 +1,3 @@
-import '../../../css/components/preloader.css';
-import '../../../css/components/sideNavAdmin.css';
-import '../../../css/pages/adminviewmembership/adminViewMembership.css';
-import { startSessionChecks } from '../../../utils/sessionUtils.js';
-import '../../components/sideNavAdmin.js';
-
-startSessionChecks();
 const cardContainer = document.getElementById('cardContainer');
   const modal = document.getElementById('editModal');
   const subscribersModal = document.getElementById('subscribersModal');
